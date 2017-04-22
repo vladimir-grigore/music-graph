a45a45ac0bb8eb206760700b40f7c8ed5a180840
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-04-21 18:14:50 -0700

    Test automatic deploy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,5 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const App = require('./App.jsx');
 
-ReactDOM.render(<App name="DEVS" />, document.getElementById('react-root'));
+ReactDOM.render(<App name="Vlad, Georgi" />, document.getElementById('react-root'));
 
