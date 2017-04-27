1de5453276988497e67b721f487a5c612dadb04c
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-04-27 16:10:15 -0700

    Add try catch error handling to spotify api calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import SpotifyAPI from './spotify_web_api.js';
 import Visualizer from './visualizer.js';
 import User from './User.jsx';
 import Toggle from './Toggle.jsx';
-
+import auth from './auth.js';
 import events from './events.js';
 
 const spotify_API = new SpotifyAPI();
@@ -50,7 +50,11 @@ class App extends Component {
     // if(this.state.logged_in) {
     //   this.addSpotifyAuthToken();
     //   const user = await spotify_API.get_current_user();
-    //   console.log("-----USER------", user);
+    //   if(user === 401 || user === 403) {
+    //     this.loginUser();
+    //   } else {
+    //     console.log("-----USER------", user);
+    //   }
     //   const playlists = await spotify_API.get_user_playlists('22kychmuozobpxyvt7upchy3q');
     //   console.log("PLAYLISTS", playlists);
 
@@ -87,17 +91,29 @@ class App extends Component {
     spotify_API.set_api_token(token);
   }
 
+  loginUser(){
+    auth.login_user().then(() => {
+      localStorage.setItem('logged-in', 'true');
+      this.addSpotifyAuthToken();
+      this.setState({ logged_in: true });
+    }).catch((err) => {
+      console.error();
+    });
+  }
+
   logoutUser(){
+    localStorage.removeItem('logged-in');
+    localStorage.removeItem('user_name');
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('user_image');
+    localStorage.removeItem('access_token');
     this.setState({ logged_in: false });
   }
 
-  loginUser(){
-    this.addSpotifyAuthToken();
-    this.setState({ logged_in: true });
-  }
   handleEventClick = (event) => {
     console.log('hi');
   }
+
   render() {
     if (this.state.open == 'open') {
       return (
--- client/src/spotify_web_api.js
+++ client/src/spotify_web_api.js
@@ -12,53 +12,81 @@ class SpotifyAPI {
   }
 
   search_artists = async function(name) {
-    const data = await this.api.searchArtists(name);
-    const artists = [];
-    // Filter artist by popularity
-    data.body.artists.items.forEach(function(item){
-      if (item.popularity > 0) {
-        artists.push(item);
-      }
-    });
+    try {
+      const data = await this.api.searchArtists(name);
+      const artists = [];
+      // Filter artist by popularity
+      data.body.artists.items.forEach(function(item){
+        if (item.popularity > 0) {
+          artists.push(item);
+        }
+      });
 
-    return artists.map(item => ({ 
-      id: item.id, 
-      name: item.name, 
-      image: item.images.length > 0 ? item.images[item.images.length - 1].url : 'nothing.jpg',
-      popularity: item.popularity
-    }));
+      return artists.map(item => ({ 
+        id: item.id, 
+        name: item.name, 
+        image: item.images.length > 0 ? item.images[item.images.length - 1].url : 'nothing.jpg',
+        popularity: item.popularity
+      }));
+    } catch(err) {
+      console.error();
+    }
   }
   
   get_albums_for_artist = async function(artistID) {
-    const data = await this.api.getArtistAlbums(artistID, {limit: 20, market: 'US'})
-    const ids = data.body.items.map(x => x.id);
-    const result = await this.api.getAlbums(ids);
-    return result.body.albums;
+    try {
+      const data = await this.api.getArtistAlbums(artistID, {limit: 20, market: 'US'})
+      const ids = data.body.items.map(x => x.id);
+      const result = await this.api.getAlbums(ids);
+      return result.body.albums;
+    } catch(err) {
+      console.error();
+    }
   }
 
   get_tracks_for_album = async function(albumID) {
-    const data = await this.api.getAlbumTracks(albumID);
-    return data.body.items;
+    try {
+      const data = await this.api.getAlbumTracks(albumID);
+      return data.body.items;
+    } catch(err) {
+      console.error();
+    }
   }
 
   get_track = async function(trackID) {
-    const data = await this.api.getTracks([trackID]);
-    return data.body;
+    try {
+      const data = await this.api.getTracks([trackID]);
+      return data.body;
+    } catch(err) {
+      console.error();
+    }
   }
 
   get_current_user = async function() {
-    const data = await this.api.getMe();
-    return data.body;
+    try {
+      const data = await this.api.getMe();
+      return data.body;
+    } catch (err) {
+      return err.statusCode;
+    }
   }
   
   get_user_playlists = async function(username) {
-    const data = await this.api.getUserPlaylists(username);
-    return data.body;
+    try {
+      const data = await this.api.getUserPlaylists(username);
+      return data.body;
+    } catch (err) {
+      return err.statusCode;
+    }
   }
 
   get_playlist = async function(userID, playlistID) {
-    const data = await this.api.getPlaylist(userID, playlistID);
-    return data.body;
+    try {
+      const data = await this.api.getPlaylist(userID, playlistID);
+      return data.body;
+    } catch (err) {
+      return err.statusCode;
+    }
   }
 
 }
