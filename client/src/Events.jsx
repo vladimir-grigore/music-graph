e31794e167ac0f77ebdc0fd667a232d4de12b436
Georgi Nikolov <gnikolov@sfu.ca>
2017-05-02 15:15:09 -0700

    Complete Events

--- client/src/Events.jsx
+++ client/src/Events.jsx
@@ -1,3 +1,9 @@
+///////////////////////EVENTS API//////////////////////////
+// events.get_artist_by_name('Metallica');
+// events.get_venue_by_name('Commodore');
+// events.get_events_by_venue_id(3816);
+///////////////////////EVENTS API//////////////////////////
+
 import React, {Component} from 'react';
 import EventsAPI from './events.js';
 import SearchBar from './SearchBar.jsx';
@@ -9,133 +15,52 @@ class Events extends Component {
     this.state = {
       isModalOpen: false,
       eventTypeSearch: '',
-      queryResults: {},
+      queryResults: [],
       eventResults: []
-      //////////////////Fake Events////////////////////
-      // fakeEnvent s: [{
-      //   Id: 2927622,
-      //   Date: '2017-05-10T00:00:00',
-      //   TicketUrl: 'http://ticketmaster.evyy.net/c/252938/264167/4272?u=http%3A%2F%2Fticketmaster.com%2Fevent%2F15005242EECB475C',
-      //   Venue: {
-      //     City: 'Baltimore ',
-      //     CountryCode: 'US',
-      //     Name: 'M&T Bank Stadium',
-      //     StateCode: 'MD'
-      //   }
-      // },
-      // {
-      //   Id: 2927623,
-      //   Date: '2017-05-12T00:00:00',
-      //   TicketUrl: 'http://ticketmaster.evyy.net/c/252938/264167/4272?u=http%3A%2F%2Fticketmaster.com%2Fevent%2F02005248D8DB65D7',
-      //   Venue: {
-      //     City: 'Philadelphia',
-      //     CountryCode: 'US',
-      //     Name: 'Lincoln Financial Field',
-      //     StateCode: 'PA'
-      //   }
-      // },
-      // {
-      //   Id: 2927624,
-      //   Date: '2017-05-14T00:00:00',
-      //   TicketUrl: 'http://ticketmaster.evyy.net/c/252938/264167/4272?u=http%3A%2F%2Fticketmaster.com%2Fevent%2F00005247E37848EF',
-      //   Venue: {
-      //     City: 'East Rutherford',
-      //     CountryCode: 'US',
-      //     Name: 'MetLife Stadium ',
-      //     StateCode: 'NJ'
-      //   }
-      // }]
-      //////////////////Fake Events////////////////////
     }
-    events.updateCallback = this.handleEventsUpdate;  // comment out when testing with fakeEnvents
   }
 
-  handleEventsUpdate = async (eventResults) => {
-    this.setState({ eventResults });
-  }
-  // BEFORE CHANGES
-  // openModal = async () => {
-  //   await this.queryEvents();
-  //   this.setState({ isModalOpen: true });
-  // }
-  // closeModal = () => {
-  //   this.setState({ isModalOpen: false })
-  // }
-  // queryEvents = async () => {
-  //   await events.get_events_by_artist_id(31754); // comment out when testing with fakeEnvents
-  // }
-  // AFTER CHANGES -->
-
-  ///////////////////////EVENTS API//////////////////////////
-  // events.get_artist_by_name(artistName);
-  // events.get_venue_by_name('Commodore');
-  // events.get_events_by_venue_id(3816);
-  // events.get_events_by_artist_id_start_end_date(31754, '2017-05-01', '2017-08-30');
-  // events.get_events_by_venue_id_start_end_date(3816, '2017-05-01', '2017-08-30');
-  ///////////////////////EVENTS API//////////////////////////
-
-  /*
-  TODO
-  0)  Get input √
-  1)  Check for Type of Query √
-  2)  Query with the input √
-  3)  Store input √
-  4)  Display input in render
-  5)  Handle Event Listener to get selected name for the next Query
-  6)  Query 2nd time to get data for that name
-  7)  Open Modal with data
-  */
-
+  // Once event button is clicked set its type to state {either Artist or Venue}
   handleEventTypeButtons = (eventType) => {
     this.setState({eventTypeSearch: eventType})
   }
 
+  // Take search input and store in state {name of either Artist or Venue}
   handleSearch = (input) => { // Get first query jamBase to get list of names & ids
-    console.log('@E - Received input is: ', input);
-    this.setState({eventTypeSearch: input});
-    console.log('@E - State of eventTypeSearch is set to :', this.state.eventTypeSearch);
-    this.queryEvents(input);
-    console.log('@E - Received search input is: ', input);
-  }
-
-  queryEvents = async (name) => { // Get a name as input and queries for a list of matching names and their ids
-    let queryResults = {};  // return from query is an object { Info: {}, Artists: [{}] } || { Info: {}, Venues: [{}] }
-    if ( input === 'Venue' ){
-      queryResults = await events.get_venue_by_name(name);
-      console.log('@E - The list of venues with events :', queryResults);
-      this.setState( { queryResults } );
+    if ( this.state.eventTypeSearch === 'Venues' ){
+      this.queryVenues(input);
     } else {
-      queryResults = await events.get_artist_by_name(name);
-      console.log('@E - The list of artists with events :', queryResults);
-      this.setState( { queryResults } );
+      this.queryArtists(input);
     }
   }
 
-  findId = (name, arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].name === name) {
-          return arr[i];
-      }
-    }
+  // First query for selecting a list of venues or artists to select from
+  queryVenues = async (name) => {
+    const queryResults = await events.get_venue_by_name(name);
+    this.setState( { queryResults: queryResults.Venues } );
   }
 
-  queryById = async (id, type) => { // Get a name as input and queries for a list of matching names and their ids
-    if ( type === 'Venue' ){
+  queryArtists = async (name) => {
+    const queryResults = await events.get_artist_by_name(name);
+    this.setState( { queryResults: queryResults.Artists } );
+  }
+
+  // Second query for displaying information in the Modal
+  queryById = async (id) => { // Get a name as input and queries for a list of matching names and their ids
+    const type = this.state.eventTypeSearch;
+    let eventResults = {};
+    if ( type === 'Venues' ){
       eventResults = await events.get_events_by_venue_id(id);
-      console.log('@E - The list of venue events with events :', eventResults);
       this.setState( { eventResults } );
     } else {
       eventResults = await events.get_events_by_artist_id(id);
-      console.log('@E - The list of artist events with events :', eventResults);
       this.setState( { eventResults } );
     }
   }
 
-  openModal = async (name) => { // 2nd query  -> name is name that is clicked
-    const queryType = this.state.eventTypeSearch;
-    const queryTarget = this.state.queryResults.queryType;  // name here is either Venues or Artists
-    const resultID = this.findId(name, queryTarget);
-    await this.queryById(resultID, queryType);
+  // 2nd query  -> id is name that is clicked
+  openModal = async (id) => {
+    await this.queryById(id);
     this.setState({ isModalOpen: true });
   }
 
@@ -144,11 +69,7 @@ class Events extends Component {
   }
 
   render() {
-
-    // TODO
-    //  render -> without list of events
-    //  else render -> with list of events to select from
-    //  check if event is being clicked -> render modal
+    // Initial State
     if ( this.state.queryResults === null ) {
       return (
         <div className='events'>
@@ -156,88 +77,75 @@ class Events extends Component {
           <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
         </div>
       )
-    } else if (this.state.queryResults !== null && (this.state.eventResults.length === 0)) {
-      const queryResultsList = Object.keys(this.state.queryResults).map(event =>
-        <div>
-          {event.name}
-        </div>
-      );
+    } else if (this.state.queryResults.length !== 0 && (this.state.eventResults.length === 0)) {
+      // When Search result is received and first query is complete save data
+      const queryResultsList = (this.state.queryResults).map(item =>
+        <EventsListClick
+          key={item.Id}
+          id={item.Id}
+          item={item}
+          currentEvent={this.state.eventTypeSearch}
+          openModal={this.openModal}
+        />);
+      // Render the list of results from first query
       return (
         <div className='events'>
           <SearchBar handleSearch={this.handleSearch} />
           <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
-          {this.queryResultsList}
+          {queryResultsList}
         </div>
       )
-    } else {  // Add modal open here
-      <div className='events'>
-        <SearchBar handleSearch={this.handleSearch} />
-        <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
-      </div>
-    }
-  }
+    } else {
+      // Open Modal when user clicks on name
+      return (
+        <div className='events'>
+          <SearchBar handleSearch={this.handleSearch} />
+          <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
+          <EventsModal isOpen={this.state.isModalOpen} onClose={this.closeModal} events={this.state.eventResults.Events} currentEvent={this.state.eventTypeSearch} />
+        </div>
+      )
+    } // End of if
+  } // End of Render
 }
 
-// // <EventsModal isOpen={this.state.isModalOpen} onClose={this.closeModal} events={this.state.eventResults.Events} /> // comment out when testing with fakeEnvents
-// if (this.state.queryResults !== null){
-//   const queryResultsList = Object.keys(this.state.queryResults).map(d =>
-//     <div>
-//       {d.name}
-//     </div>
-//   ); // comment out when testing with fakeEnvents
-//   // if(this.state.fakeEnvents.length === 0){
-//   return (
-//     <div className='events'>
-//       <SearchBar handleSearch={this.handleSearch} />
-//       <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
-//       {queryResultsList}
-//       {/*displayList of outputs from first query*/}
-//
-//       {/*<CustomForm hi={this.state.eventTypeSearch} />*/}
-//       {/*<button onClick={this.openModal}>Open Modal</button>*/}
-//     </div>
-//   )
-// } else if(this.state.eventResults.length === 0){ // comment out when testing with fakeEnvents
-//   // if(this.state.fakeEnvents.length === 0){
-//   return (
-//     <div className='events'>
-//       <SearchBar handleSearch={this.handleSearch} />
-//       <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
-//
-//       {/*displayList of outputs from first query*/}
-//
-//       {/*<CustomForm hi={this.state.eventTypeSearch} />*/}
-//       {/*<button onClick={this.openModal}>Open Modal</button>*/}
-//     </div>
-//   );
-//   return (
-//     <div className='events'>
-//       <SearchBar handleSearch={this.handleSearch} />
-//       <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
-//
-//       <div className='events-firstQuery'>
-//         {this.state.queryResults}
-//       </div>
-//
-//       {/*<CustomForm hi={this.state.eventTypeSearch} />*/}
-//       {/*<button onClick={this.openModal}>Open Modal</button>*/}
-//     </div>
-//   )
-// } else {
-//   return (
-//     <div className='events'>
-//       <SearchBar handleSearch={this.handleSearch} />
-//       <EventTypeButtons handleEventTypeButtons={this.handleEventTypeButtons} />
-//
-//       {/*displayList of outputs from first query*/}
-//
-//       {/*<CustomForm hi={this.state.eventTypeSearch} />*/}
-//       {/*<EventsModal isOpen={this.state.isModalOpen} onClose={this.closeModal} events={this.state.eventResults.Events} />
-//       <button onClick={this.openModal}>Open Modal</button>*/}
-//     </div>
-//   )
-// }
+// Wait for user to select Artist or Venue
+class EventsListClick extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+  handleClick = (e) => {
+    e.stopPropagation();
+    this.props.openModal(this.props.id);
+  }
+
+  render() {
+    if (this.props.currentEvent === 'Venues') {
+      return (
+        <div className='events-venues'>
+          <ul onClick={this.handleClick}>
+            {this.props.item.Name}
+            <li>
+              {this.props.item.City}
+              {this.props.item.State}
+              {this.props.item.Country}
+            </li>
+          </ul>
+        </div>
+      )
+    } else {
+      return (
+        <div className='events-artists'>
+          <ul onClick={this.handleClick}>
+            {this.props.item.Name}
+          </ul>
+        </div>
+      )
+    } // End of if
+  } // End of Render
+} // End of EventsListClick
+
+// Modal to display information from second query
 class EventsModal extends Component {
   constructor(props) {
     super(props);
@@ -253,50 +161,92 @@ class EventsModal extends Component {
 
   render() {
     if (this.props.isOpen === false) return null;
-    const eventResults = this.props.events;
-    const eventDetails = eventResults.map(event => {
-      let event_id, date, ticket_url, venue_name, city_name, country_code, state_code;
-      for ({ Date, TicketUrl, Venue } in event) {
+    const eventResults = this.props.events; // [{}] -> events={this.state.eventResults.Events} -> [{Artists}, {Venues}]
+    if (this.props.currentEvent === 'Venues') {
+      const eventDetails = eventResults.map(event => {  // loop through each object
+        let event_id, date, ticket_url;
+        let artist_name = [];
         event_id = event.Id;
         date = event.Date.replace(/T\d{2}:\d{2}:\d{2}/,'').trim();
         ticket_url = event.TicketUrl;
-        Object.keys(event.Venue).map(({Name, City, StateCode, CountryCode}) => {
-          venue_name = event.Venue.Name.trim();
-          city_name = event.Venue.City.trim();
-          state_code = event.Venue.StateCode.trim();
-          country_code = event.Venue.CountryCode.trim();
+        event.Artists.map(artist => {
+          artist_name.push(artist.Name.trim());
         });
-        return <CustomTable key={event_id} date={date} venue_name={venue_name} city_name={city_name} state_code={state_code} country_code={country_code} ticket_url={ticket_url} />
-      }
-    });
-    return (
-      <div>
-        <div className='events-backdrop' onClick={this.close}></div>
-        <div className='events-modal'>
-          <table id='gradient-style' summary='Venue Results'>
-            <thead>
-              <tr>
-                <th scope='col'>Date</th>
-                <th scope='col'>Venue Name</th>
-                <th scope='col'>City Name</th>
-                <th scope='col'>State</th>
-                <th scope='col'>Country</th>
-                <th scope='col'>Tickets</th>
-              </tr>
-            </thead>
-            <tbody>{eventDetails}</tbody>
-            <tfoot><tr><td colSpan='6'>Thank you for visiting our website!</td></tr></tfoot>
-          </table>
+        artist_name = artist_name.join(', ');
+        // Create a row with all the information for each event
+        return (
+          <tr key={event_id}>
+            <td>{date}</td>
+            <td>{artist_name}</td>
+            <td><a href={this.props.ticket_url}>Buy</a></td>
+          </tr>
+        )
+      }); // End of eventDetails map
+      // Create table
+      return (
+        <div>
+          <div className='events-backdrop' onClick={this.close}></div>
+          <div className='events-modal'>
+            <table id='gradient-style' summary='Venue Results'>
+              <thead>
+                <tr>
+                  <th scope='col'>Date</th>
+                  <th scope='col'>Artist Name</th>
+                  <th scope='col'>Tickets</th>
+                </tr>
+              </thead>
+              <tbody>{eventDetails}</tbody>
+              <tfoot><tr><td colSpan='3'>Thank you for visiting our website!</td></tr></tfoot>
+            </table>
+          </div>
         </div>
-      </div>
-    )
-  }
-}
+      )
+    } else {  // If its Artists
+      const eventDetails = eventResults.map(event => {
+        let event_id, date, ticket_url, venue_name, city_name, country_code, state_code;
+        for ({ Date, TicketUrl, Venue } in event) {
+          event_id = event.Id;
+          date = event.Date.replace(/T\d{2}:\d{2}:\d{2}/,'').trim();
+          ticket_url = event.TicketUrl;
+          Object.keys(event.Venue).map(({Name, City, StateCode, CountryCode}) => {
+            venue_name = event.Venue.Name.trim();
+            city_name = event.Venue.City.trim();
+            state_code = event.Venue.StateCode.trim();
+            country_code = event.Venue.CountryCode.trim();
+          });
+          return <CustomTable key={event_id} date={date} venue_name={venue_name} city_name={city_name} state_code={state_code} country_code={country_code} ticket_url={ticket_url} />
+        }
+      });
+      return (
+        <div>
+          <div className='events-backdrop' onClick={this.close}></div>
+          <div className='events-modal'>
+            <table id='gradient-style' summary='Venue Results'>
+              <thead>
+                <tr>
+                  <th scope='col'>Date</th>
+                  <th scope='col'>Venue Name</th>
+                  <th scope='col'>City Name</th>
+                  <th scope='col'>State</th>
+                  <th scope='col'>Country</th>
+                  <th scope='col'>Tickets</th>
+                </tr>
+              </thead>
+              <tbody>{eventDetails}</tbody>
+              <tfoot><tr><td colSpan='6'>Thank you for visiting our website!</td></tr></tfoot>
+            </table>
+          </div>
+        </div>
+      )
+    } // End of If
+  } // End of Render
+} // End of Modal component
 
+// Rows for each Artist
 class CustomTable extends Component {
   render(){
     return (
-      <tr>
+      <tr key={this.props.event_id}>
         <td>{this.props.date}</td>
         <td>{this.props.venue_name}</td>
         <td>{this.props.city_name}</td>
@@ -307,6 +257,8 @@ class CustomTable extends Component {
     );
   }
 }
+
+// To get information on which event is desired -> Either Venue || Artist
 class EventTypeButtons extends Component {
   formSelector = (event) => {
     const eventType = event.target.className.split(' ')[1];
@@ -316,8 +268,8 @@ class EventTypeButtons extends Component {
     return (
       <div>
         <h1>Events</h1>
-        <span className='form-venueBtn Venue' onClick={this.formSelector}>By Venue</span>
-        <span className='form-artistBtn Artist' onClick={this.formSelector}>By Artist</span>
+        <span className='form-venueBtn Venues' onClick={this.formSelector}>By Venue</span>
+        <span className='form-artistBtn Artists' onClick={this.formSelector}>By Artist</span>
       </div>
     );
   }
